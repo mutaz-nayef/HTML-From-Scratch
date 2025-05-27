b83aaa1af109fde253eb39fa0637d8303c6d011b
Mutaz Nayef <=>
2025-05-27 09:00:54 +0300

    finish page two

---
+++ src/js/mushroom-filter.js
@@ -0,0 +1,53 @@
+const cards = document.querySelectorAll(".mushroom-guide .card");
+
+const seasonalFilter = document.querySelector("#season");
+const edibleFilter = document.querySelector("#edible");
+
+const noResultFilter = document.querySelector(".no-matches");
+const currentFilters = {
+  season: "all",
+  edible: "all",
+};
+
+seasonalFilter.addEventListener("change", updateFilter);
+edibleFilter.addEventListener("change", updateFilter);
+
+function updateFilter(e) {
+  const filterType = e.target.name;
+  currentFilters[filterType] = e.target.value;
+
+  filterCards();
+}
+
+function filterCards() {
+  let hasVisibleCards = false;
+
+  cards.forEach((card) => {
+    const season = card.querySelector("[data-season]").dataset.season;
+    const edible = card.querySelector("[data-edible]").dataset.edible;
+
+    const matchesSeason = currentFilters.season === season;
+    const matchesEdible = currentFilters.edible === edible;
+
+    if (
+      (currentFilters.season === "all" || matchesSeason) &&
+      (currentFilters.edible === "all" || matchesEdible)
+    ) {
+      card.hidden = false;
+      hasVisibleCards = true;
+    } else {
+      card.hidden = true;
+    }
+    if (hasVisibleCards) {
+      noResultFilter.hidden = true;
+    } else {
+      noResultFilter.hidden = false;
+    }
+  });
+}
+
+function enableFiltering() {
+  seasonalFilter.hidden = false;
+  edibleFilter.hidden = false;
+}
+enableFiltering();
